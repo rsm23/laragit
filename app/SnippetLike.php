aa3543e4fbea804cc075529768aef14c05fe31a7
Vladimir Alabov <vladimir.alabov@bsc-ideas.com>
2017-10-13 15:48:39 +0300

    Fixing SnippetsController: optimising, refactoring, removing unused code

--- app/SnippetLike.php
+++ app/SnippetLike.php
@@ -4,8 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class SnippetLike extends Model {
 	use SoftDeletes;
 	protected $fillable = [ 'user_id', 'snippet_id' ];
+
+    /**
+     * Snippets sorted by descending likes
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function scopeMostLiked() {
+        return $this->select( 'snippet_id' )
+            ->orderBy( DB::raw( 'count(snippet_id)' ), 'DESC' )
+            ->groupBy( 'snippet_id' );
+    }
 }
