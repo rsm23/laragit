e50d2a8945fa58efec3eb8c0451a33ee77ca35a4
Tom Anderson <portlester.tom@gmail.com>
2017-10-15 23:11:52 +1100

    Finished script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 echo Installing Laragit
 echo Cloning repo...
 git clone https://github.com/rsm23/laragit.git
+cd laragit
 echo Clone completed. Installing dependencies...
 composer install
 npm install
@@ -24,3 +25,13 @@ read db_username
 
 echo Databse password:
 read db_password
+
+touch ./.env
+echo DB_CONNECTION=$db_connection >> ./.env
+echo DB_HOST=$db_host >> ./.env
+echo DB_PORT=$db_port >> ./.env
+echo DB_DATABASE=$db_database >> ./.env
+echo DB_USERNAME=$db_username >> ./.env
+echo DB_PASSWORD=$db_password >> ./.env
+
+echo Successfully installed Lavagit. Enjoy!
